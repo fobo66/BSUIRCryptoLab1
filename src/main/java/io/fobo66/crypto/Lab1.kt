2f118a7f7a06c835d7cc3ea4860d0d926b533e8c
Andrei Mukamolau <fobo66@Andreis-Air.lan>
2026-06-26 21:44:13 +0300

    Migrated to Clikt

--- src/main/java/io/fobo66/crypto/Lab1.kt
+++ src/main/java/io/fobo66/crypto/Lab1.kt
@@ -1,64 +1,56 @@
 package io.fobo66.crypto
 
-import kotlinx.cli.ArgParser
-import kotlinx.cli.ArgType
-import kotlinx.cli.default
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.main
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.help
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.transform.theme
+import com.github.ajalt.clikt.parameters.types.choice
+import com.github.ajalt.clikt.parameters.types.path
 import java.io.IOException
 import java.math.BigInteger
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.util.*
 import kotlin.io.path.readText
 
 const val RADIX = 16
 
-fun main(args: Array<String>) {
-    var clearText = "Hello World!"
+class Lab1 : CliktCommand() {
+    val inputFile by option("-f", "--file")
+        .path(mustBeReadable = true, canBeDir = false, mustExist = true)
+        .help { theme.info("Input file") }
 
-    val parser = ArgParser("lab1")
+    val key by option("-k", "--key")
+        .help { theme.info("Encryption key") }
+        .default("password")
 
-    val inputFile by parser.option(
-        ArgType.String,
-        fullName = "file",
-        shortName = "f",
-        description = "Input file"
-    )
-    val key by parser.option(
-        ArgType.String,
-        fullName = "key",
-        shortName = "k",
-        description = "Encryption key"
-    ).default("password")
-    val mode by parser.option(
-        ArgType.Choice<DESMode>(),
-        fullName = "mode",
-        shortName = "m",
-        description = "Operation mode for DES (ECB, CBC, CFB, OFB)"
-    ).default(DESMode.ECB)
+    val mode by option("-m", "--mode")
+        .choice("ECB" to DESMode.ECB, "CBC" to DESMode.CBC, "CFB" to DESMode.CFB, "OFB" to DESMode.OFB)
+        .help { theme.info("Operation mode for DES (ECB, CBC, CFB, OFB)") }
+        .default(DESMode.ECB)
 
-    try {
-        parser.parse(args)
-        if (inputFile != null) {
-            val filePath = inputFile!!
-            println("Reading cleartext from file $filePath...")
-            clearText = loadClearTextFromFile(filePath)
+    override fun run() {
+        var clearText = "Hello World!"
+
+        try {
+            if (inputFile != null) {
+                echo("Reading cleartext from file $inputFile...")
+                clearText = inputFile?.readText().orEmpty()
+            }
+            echo("Using $mode mode of operation...")
+            val encryptedText = DES.encrypt(clearText.toByteArray(), key.toByteArray(), mode)
+            val decryptedText = DES.decrypt(encryptedText, key.toByteArray(), mode)
+            printResults(clearText, encryptedText, decryptedText)
+        } catch (e: IOException) {
+            throw IllegalStateException("Failed to read cleartext from file", e)
         }
-        println("Using $mode mode of operation...")
-        val encryptedText = DES.encrypt(clearText.toByteArray(), key.toByteArray(), mode)
-        val decryptedText = DES.decrypt(encryptedText, key.toByteArray(), mode)
-        printResults(clearText, encryptedText, decryptedText)
-    } catch (e: IOException) {
-        throw IllegalStateException("Failed to read cleartext from file", e)
     }
+
+    private fun printResults(clearText: String, encryptedText: ByteArray, decryptedText: ByteArray) {
+        echo("Clear text: $clearText")
+        echo("Encrypted text: ${BigInteger(1, encryptedText).toString(RADIX)}")
+        echo("Decrypted text: ${decryptedText.toString(Charsets.UTF_8)}")
+    }
+
 }
 
-@Throws(IOException::class)
-private fun loadClearTextFromFile(filePath: String): String {
-    return Paths.get(filePath).readText()
-}
-
-private fun printResults(clearText: String, encryptedText: ByteArray, decryptedText: ByteArray) {
-    println("Clear text: $clearText")
-    println("Encrypted text: " + BigInteger(1, encryptedText).toString(RADIX))
-    println("Decrypted text: " + decryptedText.toString(Charsets.UTF_8))
-}
+fun main(args: Array<String>) = Lab1().main(args)
