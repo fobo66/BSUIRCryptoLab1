1777e67853f5af5f2b005c34eac80840d8ee3c55
Andrei Mukamolau <fobo66@Andreis-Air.lan>
2026-06-26 21:51:28 +0300

    Cleaned up test

--- src/test/kotlin/io/fobo66/crypto/Lab1Test.kt
+++ src/test/kotlin/io/fobo66/crypto/Lab1Test.kt
@@ -1,27 +1,32 @@
 package io.fobo66.crypto
 
-import java.util.*
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.spec.IvParameterSpec
+import kotlin.io.encoding.Base64
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotEquals
+
+private const val CLEARTEXT = "testtesttesttest"
+private const val KEY = "testtest"
 
 class Lab1Test {
 
     @Test
     fun `DES encryption works`() {
-        val encoder = Base64.getEncoder()
         val key = KeyGenerator.getInstance("DES").generateKey()
 //        DES output doesn't match with javax.crypto, probably because of the padding
-//        val cipher = Cipher.getInstance("DES/CBC/PKCS5Padding")
+//        val cipher = Cipher.getInstance("DES/CBC/PKCS5Padding ")
 //        cipher.init(Cipher.ENCRYPT_MODE, key, IvParameterSpec(DES.IV))
 //        val expectedEncryptionResult = cipher.doFinal(CLEARTEXT.toByteArray())
 
         val encryptionResult = DES.encrypt(CLEARTEXT.toByteArray(), key.encoded, DESMode.CBC)
-//        assertEquals(encoder.encodeToString(expectedEncryptionResult), encoder.encodeToString(encryptionResult))
-        assertNotEquals(encoder.encodeToString(CLEARTEXT.toByteArray()), encoder.encodeToString(encryptionResult))
+//        assertEquals(Base64.encode(expectedEncryptionResult), Base64.encode(encryptionResult))
+        assertNotEquals(
+            Base64.encode(CLEARTEXT.toByteArray()),
+            Base64.encode(encryptionResult)
+        )
     }
 
     @Test
@@ -33,17 +38,11 @@ class Lab1Test {
 
     @Test
     fun `DES encryption in different modes produces different results`() {
-        val encoder = Base64.getEncoder()
 
         val cleartextBytes = CLEARTEXT.toByteArray()
         val keyBytes = KEY.toByteArray()
         val encryptionResultEcb = DES.encrypt(cleartextBytes, keyBytes, DESMode.ECB)
         val encryptionResultCbc = DES.encrypt(cleartextBytes, keyBytes, DESMode.CBC)
-        assertNotEquals(encoder.encodeToString(encryptionResultEcb), encoder.encodeToString(encryptionResultCbc))
-    }
-
-    companion object {
-        private const val CLEARTEXT = "testtesttesttest"
-        private const val KEY = "testtest"
+        assertNotEquals(Base64.encode(encryptionResultEcb), Base64.encode(encryptionResultCbc))
     }
 }
